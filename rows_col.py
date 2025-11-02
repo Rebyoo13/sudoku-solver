2de523fd9f93e3cf242cdb2716b8b3d68e947c00
Rebyoo13 <rebecadinu13@yahoo.com>
2025-11-02 12:36:39 +0200

    Updated Sudoku Solver with a few helpful comments

--- rows_col.py
+++ rows_col.py
@@ -10,41 +10,37 @@ grid = [
     [0, 0, 0, 0, 8, 0, 0, 7, 9]
 ]
 
-def possible(x,y,n):
-    for i in range(0,9): 
-        if grid[i][x] == n: #verificam daca exista pe linie/orizontal
-           return False
-
-    for i in range(0,9):
-        if grid[y][i] == n: #verificam pe coloane/verticala acum
-            return False
-        
-    #cum ne uitam la fiecare numar de pe langa(dar sa fie in matricea de 3x3 ca poate sa fie si in colt)
-    #cu indicii calculam indicii astfel incat sa caute mereu de la inceputul fiecarui patratel
-    #rest rotunjit ori 3
-    x0 = (x // 3) * 3
-    y0 = (y // 3) * 3
-    for X in range(x0, x0 + 3): #limite patrat vertical
-        for Y in range(y0, y0 + 3): #limite patrat orizontal
-            if grid[Y][X] == n:
-                return False
-    return True
-
-def Print(matrix):
+def possible(x, y, n): #Python convetion is x = row, y = column
     for i in range(9):
-        print(matrix[i]) 
+        if grid[y][i] == n:
+            return False
+    for i in range(9):
+        if grid[i][x] == n:
+            return False
+    x0 = (x //3 )* 3
+    y0 = (y //3) * 3
+    for i in range(x0, x0+3):
+        for j in range(y0, y0+3):
+            if grid[j][i] == n:
+                return False
+    return True #If n is not the the row/column/3x3 matrix, 0 becomes n
 
-def solve(): #functia de backtracking
-    for y in range(9): 
-        for x in range(9): 
-            if grid[y][x] == 0: #nu poti sa ai 0, trebuie modificat
+def print_function(matrix):
+    for i in range(9):
+        print(matrix[i])
+        
+def solution():
+    for y in range(9):
+        for x in range(9):
+            if grid[y][x] == 0:
                 for n in range(1,10):
-                    if possible(x, y, n):
-                        grid[y][x] = n #va incepe cu 1 daca e posibil
-                        solve()
-                        grid[y][x] = 0 #daca e 0, va incepe iarasi, o sa dea solve atata timp cat e n mai sus
-                return 
-    Print(grid)
-    input("More?")
-
-solve()
+                    if possible(x,y,n): #Column, row, possible number
+                        grid[y][x] = n
+                        solution() #Recursion call, this is where the solution goes back to square 1(first element of matrix)
+                        '''If there isn't any viable solution(on the last recursive call), the
+                        called position or element is set to 0'''
+                        grid[y][x] = 0
+                return #We backtrack and we try the next number
+    print_function(grid)
+    
+solution()
